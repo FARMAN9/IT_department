50694293594c7bbefc181e6044edb6212af1237b
Syed Farman Ali <88277123+FARMAN9@users.noreply.github.com>
2024-10-23 22:48:18 +0530

    add:undercontruct

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./assets/components/Header/Header";
+import Main from "./assets/components/Main/main";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,8 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Main></Main>
+
     </>
   );
 }
---
+++ src/assets/components/Main/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      {/* Image */}
+      <img 
+        src="https://giffiles.alphacoders.com/158/158667.gif" 
+        alt="Placeholder" 
+        className="mb-4 bg-white rounded-lg shadow-lg transform transition-transform hover:shadow-2xl hover:scale-105"
+      />
+      {/* Heading */}
+      
+    </div>
+  );
+}
+
+export default Main;
