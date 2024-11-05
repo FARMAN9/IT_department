7314b512dc525168350cd32ae84b60717a39a17c
Syed Farman Ali <88277123+FARMAN9@users.noreply.github.com>
2024-11-05 20:59:53 +0530

    FIX:issue inresponcesness

---
+++ .history/src/assets/components/Main/main_20241105202420.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import AcademicSidebar from "../Sidebar/Sidebar";
+import Header from "../Header/Header";
+
+function Main() {
+  return (
+    <>
+      <div className="min-h-auto flex p-">
+        <AcademicSidebar />
+        <main className="flex-1 lg:ml-10 p-4">
+          {/* Your main content */}
+          <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            {/* Image */}
+            <img
+              src="https://giffiles.alphacoders.com/158/158667.gif"
+              alt="Placeholder"
+              className="mb-4 bg-white rounded-lg shadow-lg transform transition-transform hover:shadow-2xl hover:scale-105"
+            />
+            {/* Heading */}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default Main;
